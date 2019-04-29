06604dd51220fd5bc83716fae70e68bc14abc2ce
Juan Felipe Gómez Celis <juanfelipegomez.c@gmail.com>
2019-04-29 22:09:58 +0200

    Initial commit

---
+++ src/components/series/series.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const series = () => {
+  return <div>text link image</div>
+}
+
+return series
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,16 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <Image name="gatsby-astronaut.png" />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
